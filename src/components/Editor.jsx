e4377ea96dfee4ee0ec45c217cbaa548756f030e
Taylor Anderson <tganderson0@gmail.com>
2022-09-19 22:46:54 -0600

    good enough for today

---
+++ src/components/Editor.jsx
@@ -0,0 +1,101 @@
+import { createSignal, For } from "solid-js";
+import config from './config.js'
+
+const emptySection = { code: -1, name: 'Empty' };
+
+const Editor = () => {
+
+  const [mapScale, setMapScale] = createSignal(8);
+
+  const [currentSelected, setCurrentSelected] = createSignal(emptySection);
+
+  const [gridSizeX, setGridSizeX] = createSignal(1);
+  const [gridSizeY, setGridSizeY] = createSignal(1);
+
+  const [worldmap, setWorldMap] = createSignal([[emptySection]])
+
+  return (
+    <div class="h-full w-full">
+
+      <div class="w-full h-full clear">
+        <For each={worldmap()}>
+          {(row, rowIndex) => (
+            <div class="w-full block">
+              <For each={row}>
+                {(col, colIndex) => (
+                  <div style={{ width: `${100 / row.length}%`}} classList={{ 'translate-x-1/2': rowIndex % 2 === 1 }}>
+                    <div class="hexaparent">
+                      <div class="hexagon bg-secondary">
+                      </div>
+                    </div>
+                  </div>
+                  
+                )}
+              </For>
+              
+            </div>
+          )}
+        </For>
+      </div>
+
+      <div class="card fixed w-96 h-[98%] max-h-full right-2 bg-base-100 shadow-2xl overflow-y-scroll">
+        <div class="card-body">
+          <h2 class="card-title">View Width</h2>
+          <input type="range" min="1" max="20" value={mapScale()} onChange={(e) => setMapScale(e.target.value)} class="range" />
+
+          <h2 class="card-title mt-8 mb-2">Map Size</h2>
+          <div class="form-control">
+            <label class="input-group">
+              <span>X</span>
+              <input type="number" placeholder="1" value={gridSizeX()} onChange={(e) => setGridSizeX(e.target.value)} class="input input-bordered" />
+            </label>
+          </div>
+          <div class="form-control">
+            <label class="input-group">
+              <span>Y</span>
+              <input type="number" placeholder="1" value={gridSizeY()} onChange={(e) => setGridSizeY(e.target.value)} class="input input-bordered" />
+            </label>
+          </div>
+          <button type="button" class="btn btn-primary" onClick={() => {
+            const newMap = []
+            for (let i = 0; i < gridSizeY(); i++){
+              let current = []
+              for (let j = 0; j < gridSizeX(); j++){
+                current.push(emptySection)
+              }
+              newMap.push(current)
+            }
+            setWorldMap(newMap)
+          }}>Set</button>
+
+          <h2 class="card-title mt-8">Current Selection</h2>
+          <span>{currentSelected().name}</span>
+          <h2 class="card-title mt-8">Empty</h2>
+          <button class="btn" classList={{ 'btn-primary': currentSelected().code === -1 }} onClick={() => setCurrentSelected({ code: -1, name: 'Empty' })}>Empty</button>
+          <h2 class="card-title mt-8">Objects</h2>
+          <For each={Object.keys(config.objects)}>
+            {selection => <button id={`object-${selection}`} class="btn" classList={{ 'btn-primary': currentSelected().code === config.objects[selection].code }} onClick={() => setCurrentSelected(config.objects[selection])}>{selection}</button>}
+          </For>
+          <h2 class="card-title mt-8">Object Text</h2>
+          <For each={Object.keys(config.objectsText)}>
+            {selection => <button id={`text-${selection}`} class="btn" classList={{ 'btn-secondary': currentSelected().code === config.objectsText[selection].code }} onClick={() => setCurrentSelected(config.objectsText[selection])}>{selection}</button>}
+          </For>
+          <h2 class="card-title mt-8">Verbs</h2>
+          <For each={Object.keys(config.verbs)}>
+            {selection => <button id={`verb-${selection}`} class="btn" classList={{ 'btn-accent': currentSelected().code === config.verbs[selection].code }} onClick={() => setCurrentSelected(config.verbs[selection])}>{selection}</button>}
+          </For>
+          <h2 class="card-title mt-8">Properties</h2>
+          <For each={Object.keys(config.properties)}>
+            {selection => <button id={`prop-${selection}`} class="btn" classList={{ 'btn-primary': currentSelected().code === config.properties[selection].code }} onClick={() => setCurrentSelected(config.properties[selection])}>{selection}</button>}
+          </For>
+          <p></p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Editor;
---
+++ src/components/config.js
@@ -0,0 +1,152 @@
+const config = {
+    objects: {
+      Border: {
+        code: 1,
+        name: 'Border',
+      },
+      Floor: {
+        code: 2,
+        name: 'Floor',
+      },
+      Purple: {
+        code: 3,
+        name: 'Purple',
+      },
+      Grey: {
+        code: 4,
+        name: 'Grey'
+      },
+      Green: {
+        code: 5,
+        name: 'Green',
+      },
+      Blue: {
+        code: 6,
+        name: 'Blue',
+      },
+      Red: {
+        code: 7,
+        name: 'Red',
+      },
+      Brown: {
+        code: 8,
+        name: 'Brown'
+      },
+      Yellow: {
+        code: 9,
+        name: 'Yellow'
+      },
+    },
+    objectsText: {
+      I: {
+        code: 40,
+        name: 'I',
+      },
+      Word: {
+        code: 41,
+        name: 'Word',
+      },
+      Border: {
+        code: 42,
+        name: 'Border',
+      },
+      Floor: {
+        code: 43,
+        name: 'Floor',
+      },
+      Purple: {
+        code: 44,
+        name: 'Purple',
+      },
+      Grey: {
+        code: 45,
+        name: 'Grey',
+      },
+      Green: {
+        code: 46,
+        name: 'Green',
+      },
+      Blue: {
+        code: 47,
+        name: 'Blue',
+      },
+      Red: {
+        code: 48,
+        name: 'Red',
+      },
+      Brown: {
+        code: 49,
+        name: 'Brown',
+      },
+      Yellow: {
+        code: 50,
+        name: 'Yellow',
+      },
+    },
+    verbs: {
+      is: {
+        code: 60,
+        name: 'is',
+      },
+      am: {
+        code: 61,
+        name: 'am',
+      },
+      can: {
+        code: 62,
+        name: 'can',
+      },
+      and: {
+        code: 63,
+        name: 'and',
+      },
+    },
+    properties: {
+      Goal: {
+        code: 76,
+        name: 'Goal',
+      },
+      Climb: {
+        code: 70,
+        name: 'Climb',
+      },
+      Swim: {
+        code: 71,
+        name: 'Swim',
+      },
+      Burn: {
+        code: 72,
+        name: 'Burn',
+      },
+      Send: {
+        code: 80,
+        name: 'Send',
+      },
+      Stop: {
+        code: 73,
+        name: 'Stop',
+      },
+      Push: {
+        code: 74,
+        name: 'Push',
+      },
+      Pull: {
+        code: 75,
+        name: 'Pull',
+      },
+      Sink: {
+        code: 77,
+        name: 'Sink',
+      },
+      Chill: {
+        code: 78,
+        name: 'Chill',
+      },
+      Steep: {
+        code: 79,
+        name: 'Steep',
+      },
+    },
+}
+
+export default config
